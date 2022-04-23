ea57ad6e8a9e0dd314b5ba8fb734ac833f62fb43
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-04-23 18:07:30 -0400

    50% tests, working on controllers

--- src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
@@ -2,33 +2,38 @@ package com.zipcodewilmington.bakery.controllers;
 
 import com.zipcodewilmington.bakery.models.Baker;
 import com.zipcodewilmington.bakery.services.BakerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+@Controller
 public class BakerController {
+    @Autowired
     private BakerService service;
 
     public BakerController(BakerService service) {
         this.service = service;
     }
-
+@GetMapping
     public ResponseEntity<Iterable<Baker>> index() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
-
-    public ResponseEntity<Baker> show(Long id) {
+@GetMapping
+    public ResponseEntity<Baker> show(@RequestParam Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
-
+@PostMapping
     public ResponseEntity<Baker> create(Baker baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
-
-    public ResponseEntity<Baker> update(Long id, Baker baker) {
+@PutMapping
+    public ResponseEntity<Baker> update(@PathVariable Long id, @PathVariable Baker baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
-
-    public ResponseEntity<Boolean> destroy(Long id) {
+@DeleteMapping
+    public ResponseEntity<Boolean> destroy(@PathVariable Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
 }
